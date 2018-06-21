b675be5cf6e2c96d0cc35b785b5f39092a781785
andersonbellini <andersonunip@gmail.com>
2018-06-21 16:16:06 -0300

    Correct space
    correct space null

--- src/app/_services/service-helper.service.ts
+++ src/app/_services/service-helper.service.ts
@@ -14,17 +14,17 @@ export class ServiceHelpersService {
     public post<T>(api: string, url: string, data: any, addToken: boolean = true,
         contentType: ContentTypeEnum = ContentTypeEnum.JSON): Observable<T> {
         return this._http.post<T>(this.joinEndPoint(api, url), data, {withCredentials: addToken})
-            .pipe(map(this.extractData),catchError(this.handleError.bind(this)));
+            .pipe(map(this.extractData), catchError(this.handleError.bind(this)));
     }
     public postLogin<T>(api: string, url: string, data: any, addToken: boolean = true,
         contentType: ContentTypeEnum = ContentTypeEnum.JSON): Observable<T> {
         return this._http.post<T>(this.joinEndPoint(api, url), data, this.getOptions(data, contentType, addToken))
-            .pipe(map(this.extractData),catchError(this.handleError.bind(this)));
+            .pipe(map(this.extractData), catchError(this.handleError.bind(this)));
     }
 
     public get<T>(api: string, url: string, params: HttpParams, withCredentials: boolean = true): Observable<T> {
         return this._http.get<T>(this.joinEndPoint(api, url), { withCredentials: withCredentials })
-            .pipe(map(this.extractData),catchError(this.handleError.bind(this)));
+            .pipe(map(this.extractData), catchError(this.handleError.bind(this)));
     }
 
     public getNoParams<T>(api: string, url: string): Observable<T> {
@@ -40,7 +40,7 @@ export class ServiceHelpersService {
         let headers;
         headers = new HttpHeaders()
             .append('Accept', 'application/json')
-            .append('Content-Type', 'application/json');       
+            .append('Content-Type', 'application/json');
         return [{ headers: headers, params: params }];
     }
 
